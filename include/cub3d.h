d4921e44e6685ac9ba2afc7c9fa52b5fbc8dddf4
Marcus Vinicius Pimenta <57456066+viniiipimenta@users.noreply.github.com>
2023-06-01 17:36:13 -0300

    Add files via upload

--- include/cub3d.h
+++ include/cub3d.h
@@ -1,15 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpimenta <mpimenta@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/01 17:32:39 by mpimenta          #+#    #+#             */
+/*   Updated: 2023/06/01 17:35:19 by mpimenta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CUB3D_H
 #define CUB3D_H
 
-# ifdef __linux__
-#  include "../libs/mlx_linux/mlx.h"
-# else
-#  include "../libs/mlx/mlx.h"
-# endif
+#ifdef __linux__
+#include "../libs/mlx_linux/mlx.h"
+#else
+#include "../libs/mlx/mlx.h"
+#endif
 
 #include "../libs/gnl/get_next_line.h"
 #include "../libs/libft/libft.h"
-
-
 
 #endif
