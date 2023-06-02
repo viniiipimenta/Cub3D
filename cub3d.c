e264f421eef36c9ea8f5f1e0e9d5ce8c7c2b9f08
Marcus Vinicius Pimenta <57456066+viniiipimenta@users.noreply.github.com>
2023-06-02 01:09:00 -0300

    Validation map done

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 /*   By: mpimenta <mpimenta@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 17:32:24 by mpimenta          #+#    #+#             */
-/*   Updated: 2023/06/02 00:08:17 by mpimenta         ###   ########.fr       */
+/*   Updated: 2023/06/02 01:08:18 by mpimenta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int main(int argc, char **argv)
 {
+    t_game cub;
+    
     if (!validation_file(argc, argv))
         return (0);
     return (0);
