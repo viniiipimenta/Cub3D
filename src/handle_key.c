8cca90e04a9a86e63a7ea05e173d0bd472fe573c
Marcus Vinicius Pimenta <57456066+viniiipimenta@users.noreply.github.com>
2023-06-23 18:25:24 -0300

    new map and keys fixed

--- src/handle_key.c
+++ src/handle_key.c
@@ -6,7 +6,7 @@
 /*   By: mpimenta <mpimenta@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 20:02:41 by mpimenta          #+#    #+#             */
-/*   Updated: 2023/06/18 18:46:01 by mpimenta         ###   ########.fr       */
+/*   Updated: 2023/06/23 18:16:28 by mpimenta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,36 @@
 
 int	handle_key_down(int key, t_game *cub)
 {
-	if (key == 25)
+	if (key == 119)
 		cub->keys.w_pressed = 1;
-	if (key == 38)
+	if (key == 97)
 		cub->keys.a_pressed = 1;
-	if (key == 32)
+	if (key == 115)
 		cub->keys.s_pressed = 1;
-	if (key == 33)
+	if (key == 100)
 		cub->keys.d_pressed = 1;
-	if (key == 123)
+	if (key == 65361)
 		cub->keys.left_pressed = 1;
-	if (key == 124)
+	if (key == 65363)
 		cub->keys.right_pressed = 1;
-	if (key == 57)
+	if (key == 65307)
 		close_cub(cub);
 	return (0);
 }
 
 int	handle_key_up(int key, t_game *cub)
 {
-	if (key == 25)
+	if (key == 119)
 		cub->keys.w_pressed = 0;
-	if (key == 38)
+	if (key == 97)
 		cub->keys.a_pressed = 0;
-	if (key == 32)
+	if (key == 115)
 		cub->keys.s_pressed = 0;
-	if (key == 33)
+	if (key == 100)
 		cub->keys.d_pressed = 0;
-	if (key == 123)
+	if (key == 65361)
 		cub->keys.left_pressed = 0;
-	if (key == 124)
+	if (key == 65363)
 		cub->keys.right_pressed = 0;
 	return (0);
 }
